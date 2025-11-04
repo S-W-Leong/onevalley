dc2a6dce489506a0e969d8a52109eca5bc6be916
jeffwgg <wongjeff1229@gmail.com>
2025-11-05 02:25:41 +0800

    Revert "integrate zklogin and added tailwind dependency"
    This reverts commit b5c346dfb56e192137994356d5634ecf60e98d7a.

--- log.js
+++ log.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import https from 'https';
+const fs = require('fs');
+const https = require('https');
 
 const main = async () => {
     const args = process.argv.slice(2);
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    distDir: 'dist',
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+    distDir: 'dist'
 };
 
 export default nextConfig;
